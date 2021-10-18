1a8b2913a19d50d2398d324576521e04596501ec
Benoit Algourdin <benoit.algourdin@insa-lyon.fr>
2021-10-18 11:12:34 +0200

    gestion error

--- src/Model/Map.java
+++ src/Model/Map.java
@@ -41,6 +41,8 @@ public class Map extends MapInterface {
         planningRequest = new PlanningRequest();
         graphe= new HashMap<>();
     }
+
+
     public void createGraph() {
         for (Intersection inter : intersectionList) {
             HashMap<Intersection, Segment> destinations = new HashMap<>();
@@ -56,75 +58,80 @@ public class Map extends MapInterface {
             }
             graphe.put(inter, destinations);
         }
-
     }
 
-    public void loadMap(String fileName)
-    {
+    @Override
+    public void loadMap(String fileName) throws ParserConfigurationException, SAXException, IOException {
         //Test extension of XML file name
         String[] words = fileName.split(".");
-        assert(words[(words.length)-1].equals("XML") || words[(words.length)-1].equals("xml"));
+        if(!words[(words.length)-1].equals("XML") && !words[(words.length)-1].equals("xml")){
+            this.notifyObservers("Filename extension is not correct");
+        }else{
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            try {
+                // parse XML file
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document doc = db.parse(new File(fileName));
+                doc.getDocumentElement().normalize();
 
-        try {
-            // parse XML file
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            Document doc = db.parse(new File(fileName));
-            doc.getDocumentElement().normalize();
+                // get all nodes <intersection>
+                NodeList nodeListIntersection = doc.getElementsByTagName("intersection");
 
-            // get all nodes <intersection>
-            NodeList nodeListIntersection = doc.getElementsByTagName("intersection");
+                for (int temp = 0; temp < nodeListIntersection.getLength(); temp++) {
+                    Node node = nodeListIntersection.item(temp);
+                    if (node.getNodeType() == Node.ELEMENT_NODE) {
+                        Element element = (Element) node;
 
-            for (int temp = 0; temp < nodeListIntersection.getLength(); temp++) {
-                Node node = nodeListIntersection.item(temp);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
+                        // get intersection's attribute
+                        long id =  Long.parseLong(element.getAttribute("id"));
+                        double latitude = Double.parseDouble(element.getAttribute("latitude"));
+                        double longitude = Double.parseDouble(element.getAttribute("longitude"));
+                        System.out.println("unique ?"+checkUniqueIntersection(id,latitude,longitude));
+                        System.out.println("Intersection: "+id+"; latitude:"+latitude+"; longitude:"+longitude);
 
-                    // get intersection's attribute
-                    long id =  Long.parseLong(element.getAttribute("id"));
-                    double latitude = Double.parseDouble(element.getAttribute("latitude"));
-                    double longitude = Double.parseDouble(element.getAttribute("longitude"));
-                    System.out.println("unique ?"+checkUniqueIntersection(id,latitude,longitude));
-                    System.out.println("Intersection: "+id+"; latitude:"+latitude+"; longitude:"+longitude);
-
-                    if(checkUniqueIntersection(id,latitude,longitude)){
-                        intersectionList.add(new Intersection(id,latitude,longitude));
-                    }else{
-                        System.out.println("already in the list");
+                        if(checkUniqueIntersection(id,latitude,longitude)){
+                            intersectionList.add(new Intersection(id,latitude,longitude));
+                        }else{
+                            System.out.println("already in the list");
+                        }
                     }
                 }
-            }
 
-            // get all nodes <Segment>
-            NodeList nodeListSegment = doc.getElementsByTagName("segment");
+                // get all nodes <Segment>
+                NodeList nodeListSegment = doc.getElementsByTagName("segment");
 
-            for (int temp = 0; temp < nodeListSegment.getLength(); temp++) {
-                Node node = nodeListSegment.item(temp);
+                for (int temp = 0; temp < nodeListSegment.getLength(); temp++) {
+                    Node node = nodeListSegment.item(temp);
 
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
+                    if (node.getNodeType() == Node.ELEMENT_NODE) {
+                        Element element = (Element) node;
 
-                    // get intersection's attribute
-                    long destinationId =  Long.parseLong(element.getAttribute("destination"));
-                    long originId =  Long.parseLong(element.getAttribute("origin"));
-                    double length = Double.parseDouble(element.getAttribute("length"));
-                    String name = element.getAttribute("name");
+                        // get intersection's attribute
+                        long destinationId =  Long.parseLong(element.getAttribute("destination"));
+                        long originId =  Long.parseLong(element.getAttribute("origin"));
+                        double length = Double.parseDouble(element.getAttribute("length"));
+                        String name = element.getAttribute("name");
 
-                    Intersection origin = getIntersectionById(originId);
-                    Intersection destination = getIntersectionById(destinationId);
-                     if(origin != null && destination != null){
-                         segmentList.add(new Segment(origin,destination,name,length));
-                     }else{
-                         System.out.println("segment creation is impossible");
-                     }
-                    System.out.println("segment: origin:"+originId+"; destination:"+destinationId+"; length:"+length+"; name:"+name);
+                        Intersection origin = getIntersectionById(originId);
+                        Intersection destination = getIntersectionById(destinationId);
+                        if(origin != null && destination != null){
+                            segmentList.add(new Segment(origin,destination,name,length));
+                        }else{
+                            System.out.println("segment creation is impossible");
+                        }
+                        System.out.println("segment: origin:"+originId+"; destination:"+destinationId+"; length:"+length+"; name:"+name);
+                    }
                 }
+                extermIntersection = getExtremIntersection();
+                mapLoaded = true;
+            } catch (ParserConfigurationException |SAXException err){
+                this.notifyObservers("Parsing XML file failed");
+                throw err;
+            }catch( IOException err) {
+                this.notifyObservers("Opening XML file failed");
+                throw err;
             }
-            extermIntersection = getExtremIntersection();
-            mapLoaded = true;
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            e.printStackTrace();
         }
     }
 
@@ -161,67 +168,70 @@ public class Map extends MapInterface {
         return graphe;
     }
 
-    public void setGraphe(HashMap<Intersection, HashMap<Intersection, Segment>> graphe) {
-        this.graphe = graphe;
-    }
-
-    public void loadRequest(String fileName)
-    {
+    @Override
+    public void loadRequest(String fileName) throws ParserConfigurationException, SAXException, IOException {
         //Test extension of XML file name
         String[] words = fileName.split(".");
-        assert(words[(words.length)-1].equals("XML") || words[(words.length)-1].equals("xml"));
+        if(!words[(words.length)-1].equals("XML") && !words[(words.length)-1].equals("xml")){
+            this.notifyObservers("Filename extension is not correct");
+        }else {
 
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 
-        try {
-            // parse XML file
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            Document doc = db.parse(new File(fileName));
-            doc.getDocumentElement().normalize();
+            try {
+                // parse XML file
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document doc = db.parse(new File(fileName));
+                doc.getDocumentElement().normalize();
 
-            // get all nodes <intersection>
-            NodeList nodeListRequest = doc.getElementsByTagName("request");
+                // get all nodes <intersection>
+                NodeList nodeListRequest = doc.getElementsByTagName("request");
 
-            for (int temp = 0; temp < nodeListRequest.getLength(); temp++) {
-                Node node = nodeListRequest.item(temp);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
+                for (int temp = 0; temp < nodeListRequest.getLength(); temp++) {
+                    Node node = nodeListRequest.item(temp);
+                    if (node.getNodeType() == Node.ELEMENT_NODE) {
+                        Element element = (Element) node;
 
-                    // get request's attribute
-                    long pickupAddressId = Long.parseLong(element.getAttribute("pickupAddress"));
-                    Intersection pickupAddress = getIntersectionById(pickupAddressId);
-                    long deliveryAddressId = Long.parseLong(element.getAttribute("deliveryAddress"));
-                    Intersection deliveryAddress = getIntersectionById(deliveryAddressId);
-                    int pickupDuration = Integer.parseInt(element.getAttribute("pickupDuration"));
-                    int deliveryDuration = Integer.parseInt(element.getAttribute("deliveryDuration"));
+                        // get request's attribute
+                        long pickupAddressId = Long.parseLong(element.getAttribute("pickupAddress"));
+                        Intersection pickupAddress = getIntersectionById(pickupAddressId);
+                        long deliveryAddressId = Long.parseLong(element.getAttribute("deliveryAddress"));
+                        Intersection deliveryAddress = getIntersectionById(deliveryAddressId);
+                        int pickupDuration = Integer.parseInt(element.getAttribute("pickupDuration"));
+                        int deliveryDuration = Integer.parseInt(element.getAttribute("deliveryDuration"));
 
-                    System.out.println("Existing address ?");
-                    System.out.println("Request: pickupAddress:" + pickupAddressId + "; deliveryAddress:" + deliveryAddressId + "; pickupDuration: " + pickupDuration+" deliveryDuration: " + deliveryDuration + ";");
+                        System.out.println("Existing address ?");
+                        System.out.println("Request: pickupAddress:" + pickupAddressId + "; deliveryAddress:" + deliveryAddressId + "; pickupDuration: " + pickupDuration + " deliveryDuration: " + deliveryDuration + ";");
 
-                    planningRequest.addRequest(new Request(pickupAddress, pickupDuration, deliveryAddress, deliveryDuration));
+                        planningRequest.addRequest(new Request(pickupAddress, pickupDuration, deliveryAddress, deliveryDuration));
+                    }
                 }
-            }
 
-            // get the depot
-            NodeList nodeListDepot = doc.getElementsByTagName("depot");
-            for (int temp = 0; temp < nodeListDepot.getLength(); temp++) {
-                Node node = nodeListDepot.item(temp);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
+                // get the depot
+                NodeList nodeListDepot = doc.getElementsByTagName("depot");
+                for (int temp = 0; temp < nodeListDepot.getLength(); temp++) {
+                    Node node = nodeListDepot.item(temp);
+                    if (node.getNodeType() == Node.ELEMENT_NODE) {
+                        Element element = (Element) node;
 
-                    // get request's attribute
-                    long addressId = Long.parseLong(element.getAttribute("address"));
-                    String departTime = element.getAttribute("departureTime");
+                        // get request's attribute
+                        long addressId = Long.parseLong(element.getAttribute("address"));
+                        String departTime = element.getAttribute("departureTime");
 
-                    planningRequest.setStartingPoint(getIntersectionById(addressId));
-                    planningRequest.setDepartureTime(new SimpleDateFormat("HH:mm:ss").parse(departTime));
+                        planningRequest.setStartingPoint(getIntersectionById(addressId));
+                        planningRequest.setDepartureTime(new SimpleDateFormat("HH:mm:ss").parse(departTime));
 
-                    System.out.println("Depot: Starting point: "+addressId+" ; departureTime: "+departTime+";");
+                        System.out.println("Depot: Starting point: " + addressId + " ; departureTime: " + departTime + ";");
+                    }
                 }
+                planningLoaded = true;
+            } catch (ParserConfigurationException | SAXException err) {
+                this.notifyObservers("Parsing XML file failed");
+                throw err;
+            } catch (IOException err) {
+                this.notifyObservers("Opening XML file failed");
+                throw err;
             }
-            planningLoaded = true;
-        } catch (ParserConfigurationException | SAXException | IOException | ParseException e) {
-            e.printStackTrace();
         }
     }
 
@@ -264,7 +274,7 @@ public class Map extends MapInterface {
         return segmentList;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         Map map=new Map();
         map.loadMap("./data/fichiersXML2020/smallMap.xml");
         // PlanningRequest planning = new PlanningRequest();
--- src/Model/XML/MapInterface.java
+++ src/Model/XML/MapInterface.java
@@ -3,13 +3,17 @@ package Model.XML;
 import Model.Intersection;
 import Model.Segment;
 import Model.Tour;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Observable;
 
 public abstract class MapInterface extends Observable {
 
-    public abstract void loadMap(String fileName);
+    public abstract void loadMap(String fileName) throws ParserConfigurationException, SAXException, IOException;
+    public abstract void loadRequest(String fileName) throws ParserConfigurationException, SAXException, IOException;
     public abstract Intersection getIntersectionById(long id);
     public abstract ArrayList<Intersection>  getIntersectionList();
     public abstract ArrayList<Segment>  getSegmentList();
