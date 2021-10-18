7b4e7960fe99af994c6eefd0039f5281a0d62932
gregoire de lambertye <gdelambertye03@gmail.com>
2021-10-18 10:05:41 +0200

    change observable

--- src/Model/Tour.java
+++ src/Model/Tour.java
@@ -1,13 +1,10 @@
 package Model;
 
-import javax.swing.*;
 import java.util.ArrayList;
-import java.util.Observable;
 
-public class Tour extends Observable {
+public class Tour{
     private ArrayList<Segment> orderedSegmentList;
     private ArrayList<Intersection> orderedIntersectionList;
-    private Map map;
 
     public Tour() {
         orderedSegmentList = new ArrayList<Segment>();
@@ -20,10 +17,6 @@ public class Tour extends Observable {
 
     public ArrayList<Intersection> getOrderedIntersectionList() {
         return orderedIntersectionList;
-    }
-
-    public Map getMap() {
-        return map;
     }
 
     public void setOrderedSegmentList(ArrayList<Segment> orderedSegmentList) {
