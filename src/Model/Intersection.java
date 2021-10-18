e635d6e139d34f8068ea85dd353f7c15259854db
Tushita Ramkarn <tushitaramkaran@gmail.com>
2021-10-18 19:20:44 +0200

    Calcul distance ihm

--- src/Model/Intersection.java
+++ src/Model/Intersection.java
@@ -25,4 +25,21 @@ public class Intersection {
         return longitude;
     }
 
+    public static double calculDis(Intersection i1, Intersection i2){ //Calcul distance à vol d'oiseau
+
+        double R = 6371e3; //Rayon de la terre
+        double res = 0;
+
+        double lat1= i1.latitude * Math.PI/180; //Conversion en rad
+        double lat2= i2.latitude * Math.PI/180;
+        double deltaLat = (i2.latitude-i1.latitude) * Math.PI/180;
+        double deltaLong = (i2.longitude-i1.longitude) * Math.PI/180;
+
+        double a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(deltaLong/2) * Math.sin(deltaLong/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); //Distance angulaire
+
+        res = R * c; // in metres
+        return res;
+    }
+
 }
--- src/ihm/windowMap/MapPanel.java
+++ src/ihm/windowMap/MapPanel.java
@@ -1,5 +1,7 @@
 package ihm.windowMap;
 
+import Model.Intersection;
+import Model.Segment;
 import Model.XML.MapFactory;
 import Model.XML.MapInterface;
 
@@ -25,9 +27,65 @@ public class MapPanel extends JPanel implements MouseListener
 
     public void paint(Graphics g)
     {
-        Graphics2D graphic2d = (Graphics2D) g;
-        graphic2d.setColor(Color.BLUE);
-        graphic2d.fillRect(100, 50, 60, 80);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.red);
+        for(Intersection i: createdMap.getIntersectionList())
+        {
+            paintIntersection(g2d,i);
+        }
+        for(Segment s: createdMap.getSegmentList())
+        {
+            paintSegment(g2d,s);
+        }
+
+    }
+
+    public double[] latLonToOffsets( double latitudeOrigin, double longitudeOrigin, double latitude, double longitude, double mapWidth, double mapHeight) {
+        double radius = mapWidth / (2 * Math.PI);
+
+
+        double latRad = ((latitude) * Math.PI) / 180;
+        double lonRad = ((longitude+longitudeOrigin) * Math.PI) / 180;
+
+        double x = lonRad * radius;
+
+        double yFromEquator = radius * Math.log(Math.tan(Math.PI / 4 + latRad / 2));
+        double y = mapHeight / 2 - yFromEquator;
+
+        double res [] = {x,y};
+
+        return res;
+    }
+
+    public void paintIntersection(Graphics2D g, Intersection intersection)
+    {
+
+        g.setColor(Color.white);
+        double latitude= intersection.getLatitude();
+        double longitude= intersection.getLongitude();
+        double[] pixelCoords= latLonToOffsets( 0, 0, latitude, longitude, Frame.width,Frame.height);
+        double pixelX= pixelCoords[0];
+        double pixelY= pixelCoords[1];
+        g.drawOval((int)pixelX,(int) pixelY, 1,1);
+
+
+    }
+    public void paintSegment(Graphics2D g, Segment segment)
+    {
+        g.setColor(Color.white);
+        Intersection origin= segment.getOrigin();
+        Intersection destination= segment.getDestination();
+        double originLat= origin.getLatitude();
+        double originLong= origin.getLongitude();
+        double destinationLat= destination.getLatitude();
+        double destinationLong= destination.getLongitude();
+        double[] pixelCoordsOrigin= latLonToOffsets( 0, 0, originLat, originLong, Frame.width,Frame.height);
+        double[] pixelCoordsDestination= latLonToOffsets( 0, 0,destinationLat, destinationLong,Frame.width,Frame.height);
+        double originPixelX= pixelCoordsOrigin[0];
+        double originPixelY= pixelCoordsOrigin[1];
+        double destinationPixelX= pixelCoordsDestination[0];
+        double destinationPixelY= pixelCoordsDestination[1];
+        g.drawLine((int)originPixelX,(int)originPixelY,(int)destinationPixelX,(int)destinationPixelY);
 
     }
 
