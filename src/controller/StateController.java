56334a9be4a34fbf734607d5949f3d523dec9a23
gregoire de lambertye <gdelambertye03@gmail.com>
2021-10-20 10:34:44 +0200

    fix nothing

--- src/controller/StateController.java
+++ src/controller/StateController.java
@@ -2,5 +2,5 @@ package controller;
 
 public interface StateController {
     void action(Controller context);
-    void nextState(Controller controller);
+    void nextState(Controller context);
 }
