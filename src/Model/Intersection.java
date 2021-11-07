51a096512d1940ec05a9abae3ea847d102680d4e
Benoit Algourdin <benoit.algourdin@insa-lyon.fr>
2021-11-07 13:42:31 +0100

    Add javadoc to model

--- src/Model/Intersection.java
+++ src/Model/Intersection.java
@@ -3,10 +3,27 @@ package Model;
 import java.util.Objects;
 
 public class Intersection {
+    /**
+     * id of an intersection
+     */
     private long id;
+
+    /**
+     * latitude of an intersection
+     */
     private double latitude;
+
+    /**
+     * longitude of an intersection
+     */
     private double longitude;
 
+    /**
+     * Constructor
+     * @param id
+     * @param latitude
+     * @param longitude
+     */
     public Intersection(long id, double latitude, double longitude){
         this.id=id;
         this.latitude=latitude;
@@ -16,14 +33,24 @@ public class Intersection {
     public Intersection() {
     }
 
+    //----------- Getters -----------
+    /**
+     * @return id
+     */
     public long getId(){
         return id;
     }
 
+    /**
+     * @return latitude
+     */
     public double getLatitude() {
         return latitude;
     }
 
+    /**
+     * @return longitude
+     */
     public double getLongitude() {
         return longitude;
     }
@@ -46,19 +73,27 @@ public class Intersection {
         return Objects.hash(id,latitude,longitude);
     }
 
-    public static double calculDis(Intersection i1, Intersection i2){ //Calcul distance à vol d'oiseau
-        double R = 6371e3; //Rayon de la terre
+
+    /**
+     * Calculate the distance bewteen 2 intersections
+     * @param i1 Intersection 1
+     * @param i2 Intersection 2
+     * @return res the distance between i1 and i2
+     */
+    public static double calculDis(Intersection i1, Intersection i2){
+        double R = 6371e3; // earth radius
         double res = 0;
 
-        double lat1= i1.latitude * Math.PI/180; //Conversion en rad
+        double lat1= i1.latitude * Math.PI/180; //radian conversion
         double lat2= i2.latitude * Math.PI/180;
         double deltaLat = Math.abs((i2.latitude-i1.latitude) * Math.PI/180);
         double deltaLong = Math.abs((i2.longitude-i1.longitude) * Math.PI/180);
 
-        double a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(deltaLong/2) * Math.sin(deltaLong/2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); //Distance angulaire
+        double a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) + Math.cos(lat1) * Math.cos(lat2)
+                    * Math.sin(deltaLong/2) * Math.sin(deltaLong/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); // Angular distance
 
-        res = R * c; // in metres
+        res = R * c; // in meters
         return res;
     }
 }
--- src/Model/Tour.java
+++ src/Model/Tour.java
@@ -4,23 +4,25 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Tour{
+    /**
+     * An ordered list of segment to compute the tour
+     */
     @Deprecated
     private LinkedList<Segment> orderedSegmentList;
+
+    /**
+     * An ordered list of path to compute the tour
+     */
     private LinkedList<Path> orderedPathList;
+
     private HashMap<Intersection, Integer> originPathMap;
     private HashMap<Intersection, Integer> destinationPathMap;
 
 
-    public Path findPathOrigin(Address origin){
-        Path path = orderedPathList.get(originPathMap.get(origin));
-        return path;
-    }
-
-    public Path findPathDestination(Address destination){
-        Path path = orderedPathList.get(destinationPathMap.get(destination));
-        return path;
-    }
-
+    /**
+     * Constructor
+     * @param orderedPathList
+     */
     public Tour(LinkedList<Path> orderedPathList) {
         orderedSegmentList = new LinkedList<>();
         originPathMap = new HashMap<>();
@@ -34,11 +36,21 @@ public class Tour{
             i++;
         }
     }
+
+    /**
+     * @return orderedSegmentList
+     */
     @Deprecated
     public LinkedList<Segment> getOrderedSegmentList() {
         return orderedSegmentList;
     }
 
+    /**
+     * Replace the old path with 2 new paths
+     * @param oldPath
+     * @param newPath1
+     * @param newPath2
+     */
     public void replaceOldPath(Path oldPath, Path newPath1, Path newPath2){
         if (this.orderedPathList.contains(oldPath)){
             int index= this.orderedPathList.indexOf(oldPath);
@@ -50,11 +62,36 @@ public class Tour{
         }
     }
 
+    /**
+     * @return orderedPathList
+     */
     public LinkedList<Path> getOrderedPathList() {
         return orderedPathList;
     }
 
+    /**
+     * Set the orderedSegmentList
+     * @param orderedSegmentList
+     */
     public void setOrderedSegmentList(LinkedList<Segment> orderedSegmentList) {
         this.orderedSegmentList = orderedSegmentList;
+    }
+
+    /**
+     * @param origin
+     * @return path
+     */
+    public Path findPathOrigin(Address origin){
+        Path path = orderedPathList.get(originPathMap.get(origin));
+        return path;
+    }
+
+    /**
+     * @param destination
+     * @return path
+     */
+    public Path findPathDestination(Address destination){
+        Path path = orderedPathList.get(destinationPathMap.get(destination));
+        return path;
     }
 }
