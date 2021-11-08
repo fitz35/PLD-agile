cea9252e876c7afdc82b5ed6b2efd65b7d89b8f5
fitz35 <clement@lahoche.info>
2021-11-08 08:05:15 +0100

    Revert "commit in order to reverse the previous one"
    This reverts commit bcbb498a1b35d80d4b80298f67178612394549e7.

--- src/controller/command/DeleteRequestCmd.java
+++ src/controller/command/DeleteRequestCmd.java
@@ -8,11 +8,6 @@ import java.util.List;
 public class DeleteRequestCmd implements Command {
     private Map map;
     private Address addressToDelete;
-    private Address newPickup;
-    private Address newDelivery;
-    private Address beforNewPickup;
-    private Address beforNewDelivery;
-
 /**
  * ajouter tt les attibut de la methode addNewRequest pour le undo et etre capable de remettre tt ds l'ordre
  * 
@@ -25,11 +20,6 @@ public class DeleteRequestCmd implements Command {
 
     public DeleteRequestCmd(Map map, Address addressToDelete) {
         this.map = map;
-        List<Address> addresses = map.addressForUndoDelete(addressToDelete);
-        newPickup = addresses.get(1);
-        newDelivery = addresses.get(2);
-        beforNewPickup = addresses.get(3);
-        beforNewDelivery = addresses.get(4);
         this.addressToDelete = addressToDelete;
     }
 
@@ -43,6 +33,7 @@ public class DeleteRequestCmd implements Command {
     public void undoCommand() {
 
         //add request => must find a way to store all the needed address
-        map.addRequest(addresses.get(0),addresses.get(1),addresses.get(2),addresses.get(3));
+        List<Address> addresses = map.addressForUndoDelete(addressToDelete);
+
     }
 }
